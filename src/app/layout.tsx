360c3907b45e0d981dc76757151e070ab9554b8d
Parth Raval <parthraval217@gmail.com>
2024-04-17 19:13:11 -0400

    2024041702: Implemented Navbar and Footer.. for the same created two components – Navbar, and Footer.. added custom classes.. updated layout.. created a constant file for the data.. Hero.tsx: fixed image overlay design issue.. CustomButton.tsx and index.ts: added new property – btnType..

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 
 import "./globals.css";
+import { Footer, Navbar } from "@/components";
 
 export const metadata: Metadata = {
   title: "Autoshow",
@@ -15,7 +16,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="relative">
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
